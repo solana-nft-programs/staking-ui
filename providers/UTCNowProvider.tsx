d359d079095d5d1a4241d49417cd41bf05536bf1
jpbogle <jpbogle22@gmail.com>
2022-05-26 18:38:10 -0700

    Add clock drift warning

--- providers/UTCNowProvider.tsx
+++ providers/UTCNowProvider.tsx
@@ -1,31 +1,62 @@
-import type { ReactChild } from 'react'
+import { Connection } from '@solana/web3.js'
+import { ReactChild, useMemo } from 'react'
 import React, { useContext, useEffect, useState } from 'react'
+import { useEnvironmentCtx } from './EnvironmentProvider'
 
+const CLOCK_DRIFT_WARNING_THRESHOLD_SECONDS = 60 * 5
 export interface UTCNowContextValues {
   UTCNow: number
+  clockDrift: number | undefined
 }
 
 const UTCNowContext: React.Context<UTCNowContextValues> =
   React.createContext<UTCNowContextValues>({
     UTCNow: Math.floor(Date.now() / 1000),
+    clockDrift: 0,
   })
 
+const getSolanaClock = async (
+  connection: Connection
+): Promise<number | null> => {
+  const epochInfo = await connection.getEpochInfo()
+  const blockTimeInEpoch = await connection.getBlockTime(epochInfo.absoluteSlot)
+  return blockTimeInEpoch
+}
+
 export function UTCNowProvider({ children }: { children: ReactChild }) {
+  const { connection, environment } = useEnvironmentCtx()
   const [UTCNow, setUTCNow] = useState(Date.now() / 1000)
-  useEffect(() => {
+  const [clockDrift, setClockDrift] = useState<number | undefined>(undefined)
+
+  useMemo(() => {
     const interval = setInterval(
-      (function expirationStringUpdate() {
-        setUTCNow(Math.floor(Date.now() / 1000))
-        return expirationStringUpdate
+      (function fetchInterval(): () => void {
+        setUTCNow(Date.now() / 1000)
+        // setUTCNow(UTCNow + 1)
+        return fetchInterval
       })(),
       1000
     )
     return () => clearInterval(interval)
-  })
+  }, [Math.floor(Date.now() / 1000)])
+
+  useMemo(async () => {
+    const solanaClock = await getSolanaClock(connection)
+    if (
+      solanaClock &&
+      Math.abs(Date.now() / 1000 - solanaClock) >
+        CLOCK_DRIFT_WARNING_THRESHOLD_SECONDS
+    ) {
+      setClockDrift(Date.now() / 1000 - solanaClock)
+      setUTCNow(solanaClock)
+    }
+  }, [environment])
+
   return (
     <UTCNowContext.Provider
       value={{
         UTCNow,
+        clockDrift,
       }}
     >
       {children}
