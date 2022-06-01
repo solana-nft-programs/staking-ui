416e15cd5f96182b85caea99e3ffdf3712183ce4
jpbogle <jpbogle22@gmail.com>
2022-06-01 00:29:52 -0400

    Stake pool authority offset

--- hooks/useStakePoolsByAuthority.tsx
+++ hooks/useStakePoolsByAuthority.tsx
@@ -1,9 +1,61 @@
 import { useEnvironmentCtx } from 'providers/EnvironmentProvider'
 import { useWalletId } from './useWalletId'
 import { AccountData } from '@cardinal/common'
-import { StakePoolData } from '@cardinal/staking/dist/cjs/programs/stakePool'
+import {
+  StakePoolData,
+  STAKE_POOL_ADDRESS,
+  STAKE_POOL_IDL,
+} from '@cardinal/staking/dist/cjs/programs/stakePool'
 import { useQuery } from 'react-query'
-import { getStakePoolsByAuthority } from '@cardinal/staking/dist/cjs/programs/stakePool/accounts'
+import { Connection, PublicKey } from '@solana/web3.js'
+import { BorshAccountsCoder, utils } from '@project-serum/anchor'
+
+export const getStakePoolsByAuthority = async (
+  connection: Connection,
+  user: PublicKey
+): Promise<AccountData<StakePoolData>[]> => {
+  const programAccounts = await connection.getProgramAccounts(
+    STAKE_POOL_ADDRESS,
+    {
+      filters: [
+        {
+          memcmp: {
+            offset: 0,
+            bytes: utils.bytes.bs58.encode(
+              BorshAccountsCoder.accountDiscriminator('stakePool')
+            ),
+          },
+        },
+        {
+          memcmp: {
+            offset: 17,
+            bytes: user.toBase58(),
+          },
+        },
+      ],
+    }
+  )
+  const stakePoolDatas: AccountData<StakePoolData>[] = []
+  const coder = new BorshAccountsCoder(STAKE_POOL_IDL)
+  programAccounts.forEach((account) => {
+    try {
+      const stakePoolData: StakePoolData = coder.decode(
+        'stakePool',
+        account.account.data
+      )
+      if (stakePoolData) {
+        stakePoolDatas.push({
+          ...account,
+          parsed: stakePoolData,
+        })
+      }
+      // eslint-disable-next-line no-empty
+    } catch (e) {}
+  })
+  return stakePoolDatas.sort((a, b) =>
+    a.pubkey.toBase58().localeCompare(b.pubkey.toBase58())
+  )
+}
 
 export const useStakePoolsByAuthority = () => {
   const { secondaryConnection } = useEnvironmentCtx()
