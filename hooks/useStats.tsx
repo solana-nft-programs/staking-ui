da249fe9eb547e7eaa649261d2901d369882044d
Giannis Chatziveroglou <gchatz@mit.edu>
2022-06-27 02:01:38 +0300

    Add total pools stat

--- hooks/useStats.tsx
+++ hooks/useStats.tsx
@@ -21,6 +21,7 @@ export const useStats = () => {
       const statsNames = [
         'total-active-staked-tokens',
         'total-active-stake-entries',
+        'total-stake-pools',
       ]
       const statsData = await Promise.all(
         statsNames.map((name) => getStatsEntry(secondaryConnection, name))
@@ -52,5 +53,9 @@ const statsNameMapping = [
   {
     key: 'total-active-stake-entries',
     displayName: 'Total Staked Entries',
+  },
+  {
+    key: 'total-stake-pools',
+    displayName: 'Total Stake Pools',
   },
 ]
