cad1527c77fd363ff35bbffdb1e44b13b117e4d7
Giannis Chatziveroglou <gchatz@mit.edu>
2022-06-28 18:56:26 +0300

    Total number of tokens in pool api

---
+++ pages/api/activeTokensInPool.ts
@@ -0,0 +1,52 @@
+import {
+  getActiveStakeEntriesForPool,
+  getAllStakeEntries,
+} from '@cardinal/staking/dist/cjs/programs/stakePool/accounts'
+import { Connection } from '@solana/web3.js'
+import { firstParam, tryPublicKey } from 'common/utils'
+import type { NextApiHandler } from 'next'
+import { ENVIRONMENTS } from 'providers/EnvironmentProvider'
+
+interface GetResponse {
+  poolId?: string
+  tokens?: string
+  error?: string
+}
+
+const get: NextApiHandler<GetResponse> = async (req, res) => {
+  const { cluster: clusterParam, stakePool: stakePoolId } = req.query
+  const foundEnvironment = ENVIRONMENTS.find(
+    (e) => e.label === (firstParam(clusterParam) || 'mainnet-beta')
+  )
+  const connection = new Connection(foundEnvironment!.primary)
+  const stakePoolPubkey = await tryPublicKey(stakePoolId)
+  if (stakePoolPubkey) {
+    const activeTokens = await getActiveStakeEntriesForPool(
+      connection,
+      stakePoolPubkey
+    )
+    res.status(200).send({
+      poolId: stakePoolPubkey.toString(),
+      tokens: String(activeTokens.length),
+    })
+  } else {
+    res.status(200).send({
+      error: 'Invalid stake pool ID provided',
+    })
+  }
+}
+
+const index: NextApiHandler<GetResponse> = async (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Methods', '*')
+  response.setHeader('Access-Control-Allow-Headers', '*')
+  response.setHeader('Access-Control-Allow-Credentials', 'true')
+  if (request.method === 'GET') return get(request, response)
+  throw new Error(`Unexpected method ${request.method}`)
+}
+
+const sleep = (time: number) => {
+  return new Promise((resolve) => setTimeout(resolve, Math.ceil(time * 1000)))
+}
+
+export default index
