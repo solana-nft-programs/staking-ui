b7617083cedad4ff79c0f98c0a633b34ef641738
Brahim <78970738+BrahimBM@users.noreply.github.com>
2022-08-06 00:15:27 +0200

    add and edit ThorNode pools
    edit first pool
    add second pool

--- api/mapping.ts
+++ api/mapping.ts
@@ -755,27 +755,52 @@ export const stakePoolMetadatas: StakePoolMetadata[] = [
   },
   {
     name: 'thornode-elite',
-    displayName: 'ThorNode RPCs',
+    displayName: 'Elite Pass',
     stakePoolAddress: new PublicKey(
       'J79xX6CYxXZzXQSy6g6hkBnnfFs7pRDjqy4nFKjnXvu9'
     ),
-    websiteUrl: 'https://thornode.io',
+    websiteUrl: 'https://stake.thornode.io',
     receiptType: ReceiptType.Original,
     maxStaked: 33, // update with collection size
     imageUrl:
-      'https://creator-hub-prod.s3.us-east-2.amazonaws.com/thornode_pfp_1656943062647.gif',
+      'https://v4pnvyuwfsaiymwlmqr4mvemptw2lmqyrwmilxg2prlw7qrqf3qq.arweave.net/rx7a4pYsgIwyy2QjxlSMfO2lshiNmIXc2nxXb8IwLuE?ext=gif',
     tokenStandard: TokenStandard.NonFungible,
     hideAllowedTokens: false,
-    hidden: true,
+    hidden: false,
     styles: {
-      fontFamily: 'Industry, sans-serif',
+      fontFamily: 'anton',
       fontWeight: 500,
     },
     colors: {
-      primary: '#000000',
-      secondary: '#4da1de',
-      accent: '#1fcfb11c',
-      fontColor: '#FFFFFF',
+      primary: '#040a05',
+      secondary: '#40834b',
+      accent: 'rgba(31,207,101,0.11)',
+      fontColor: '#e8e8e8',
+    },
+  },
+  {
+    name: 'thornode-classic',
+    displayName: 'Classic Pass',
+    stakePoolAddress: new PublicKey(
+      '54LKhj2FY5Vg5uR6NBEQ4SiLJNRYCUX1oWxTZcUXZ8cE'
+    ),
+    websiteUrl: 'https://stake.thornode.io',
+    receiptType: ReceiptType.Original,
+    maxStaked: 300, // update with collection size
+    imageUrl:
+      'https://i.ibb.co/fxZqZWR/lossy-compressed-3.gif',
+    tokenStandard: TokenStandard.NonFungible,
+    hideAllowedTokens: false,
+    hidden: false,
+    styles: {
+      fontFamily: 'anton',
+      fontWeight: 500,
+    },
+    colors: {
+      primary: '#0e1b3a',
+      secondary: '#b287e1',
+      accent: 'rgba(31,207,101,0.11)',
+      fontColor: '#e8e8e8',
     },
   },
 ]
