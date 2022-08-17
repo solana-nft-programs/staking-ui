36cd8f29f02be2c44b845050d01693cbc1139e6a
Kollan House <kollanh@gmail.com>
2022-08-17 16:48:18 -0700

    fix: remove more sentry

--- pages/_error.js
+++ pages/_error.js
@@ -16,7 +16,6 @@
  *  - https://reactjs.org/docs/error-boundaries.html
  */
 
-import * as Sentry from '@sentry/nextjs';
 import NextErrorComponent from 'next/error';
 
 const CustomErrorComponent = props => {
@@ -30,7 +29,6 @@ const CustomErrorComponent = props => {
 CustomErrorComponent.getInitialProps = async contextData => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
-  await Sentry.captureUnderscoreErrorException(contextData);
 
   // This will contain the status code of the response
   return NextErrorComponent.getInitialProps(contextData);
