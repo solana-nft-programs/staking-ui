e094a09b650424e34dd179ad02a9816a2d06ac30
FlorianWeise <florianweise@gmx.de>
2022-09-05 14:51:04 +0200

    Update index.tsx

--- pages/[stakePoolId]/index.tsx
+++ pages/[stakePoolId]/index.tsx
@@ -1,65 +1,61 @@
+import { DisplayAddress } from '@cardinal/namespaces-components'
 import {
+  claimRewards,
   createStakeEntryAndStakeMint,
   stake,
   unstake,
-  claimRewards,
 } from '@cardinal/staking'
+import { RewardDistributorKind } from '@cardinal/staking/dist/cjs/programs/rewardDistributor'
 import { ReceiptType } from '@cardinal/staking/dist/cjs/programs/stakePool'
+import { Switch } from '@headlessui/react'
+import type { Wallet } from '@metaplex/js'
+import { darken, lighten } from '@mui/material'
+import { BN } from '@project-serum/anchor'
+import * as splToken from '@solana/spl-token'
 import { useWallet } from '@solana/wallet-adapter-react'
-import { PublicKey, Signer, Transaction } from '@solana/web3.js'
+import { useWalletModal } from '@solana/wallet-adapter-react-ui'
+import type { Signer, Transaction } from '@solana/web3.js'
+import { PublicKey } from '@solana/web3.js'
+import { defaultSecondaryColor, TokenStandard } from 'api/mapping'
+import { executeAllTransactions } from 'api/utils'
+import { Footer } from 'common/Footer'
 import { Header } from 'common/Header'
-import Head from 'next/head'
-import { useEnvironmentCtx } from 'providers/EnvironmentProvider'
-import { useEffect, useState } from 'react'
-import { Wallet } from '@metaplex/js'
 import { LoadingSpinner } from 'common/LoadingSpinner'
 import { notify } from 'common/Notification'
-import { contrastColorMode, pubKeyUrl, secondstoDuration } from 'common/utils'
+import { QuickActions } from 'common/QuickActions'
+import { MouseoverTooltip } from 'common/Tooltip'
 import {
   formatAmountAsDecimal,
   formatMintNaturalAmountAsDecimal,
-  getMintDecimalAmountFromNatural,
   parseMintNaturalAmountFromDecimal,
 } from 'common/units'
-import { BN } from '@project-serum/anchor'
-import {
-  StakeEntryTokenData,
-  useStakedTokenDatas,
-} from 'hooks/useStakedTokenDatas'
-import { useRewardDistributorData } from 'hooks/useRewardDistributorData'
-import { useRewards } from 'hooks/useRewards'
-import { useRewardMintInfo } from 'hooks/useRewardMintInfo'
+import { contrastColorMode, pubKeyUrl, secondstoDuration } from 'common/utils'
 import { AllowedTokens } from 'components/AllowedTokens'
-import { useStakePoolEntries } from 'hooks/useStakePoolEntries'
-import { useStakePoolData } from 'hooks/useStakePoolData'
-import { useStakePoolMaxStaked } from 'hooks/useStakePoolMaxStaked'
-import {
-  AllowedTokenData,
-  useAllowedTokenDatas,
-} from 'hooks/useAllowedTokenDatas'
-import { useStakePoolMetadata } from 'hooks/useStakePoolMetadata'
-import { defaultSecondaryColor, TokenStandard } from 'api/mapping'
-import { Footer } from 'common/Footer'
-import { DisplayAddress } from '@cardinal/namespaces-components'
+import type { AllowedTokenData } from 'hooks/useAllowedTokenDatas'
+import { useAllowedTokenDatas } from 'hooks/useAllowedTokenDatas'
+import { usePoolAnalytics } from 'hooks/usePoolAnalytics'
+import { useRewardDistributorData } from 'hooks/useRewardDistributorData'
 import { useRewardDistributorTokenAccount } from 'hooks/useRewardDistributorTokenAccount'
 import { useRewardEntries } from 'hooks/useRewardEntries'
-import { Switch } from '@headlessui/react'
-import { FaInfoCircle } from 'react-icons/fa'
-import { MouseoverTooltip } from 'common/Tooltip'
-import { useUTCNow } from 'providers/UTCNowProvider'
-import { useWalletModal } from '@solana/wallet-adapter-react-ui'
-import { executeAllTransactions } from 'api/utils'
-import { RewardDistributorKind } from '@cardinal/staking/dist/cjs/programs/rewardDistributor'
-import { useRouter } from 'next/router'
-import { lighten, darken } from '@mui/material'
-import { QuickActions } from 'common/QuickActions'
-import * as splToken from '@solana/spl-token'
-import { usePoolAnalytics } from 'hooks/usePoolAnalytics'
+import { useRewardMintInfo } from 'hooks/useRewardMintInfo'
+import { useRewards } from 'hooks/useRewards'
 import { useRewardsRate } from 'hooks/useRewardsRate'
+import type { StakeEntryTokenData } from 'hooks/useStakedTokenDatas'
+import { useStakedTokenDatas } from 'hooks/useStakedTokenDatas'
+import { useStakePoolData } from 'hooks/useStakePoolData'
+import { useStakePoolEntries } from 'hooks/useStakePoolEntries'
+import { useStakePoolMaxStaked } from 'hooks/useStakePoolMaxStaked'
+import { useStakePoolMetadata } from 'hooks/useStakePoolMetadata'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEnvironmentCtx } from 'providers/EnvironmentProvider'
+import { useUTCNow } from 'providers/UTCNowProvider'
+import { useEffect, useState } from 'react'
+import { FaInfoCircle } from 'react-icons/fa'
 
 function Home() {
   const router = useRouter()
-  const { connection, environment } = useEnvironmentCtx()
+  const { connection, environment, customHostname } = useEnvironmentCtx()
   const wallet = useWallet()
   const walletModal = useWalletModal()
   const { data: stakePool, isFetched: stakePoolLoaded } = useStakePoolData()
@@ -99,6 +95,7 @@ function Home() {
     return
   }
 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     stakePoolMetadata?.tokenStandard &&
       setShowFungibleTokens(
@@ -233,6 +230,7 @@ function Home() {
         stakedTokenDatas.refetch()
         allowedTokenDatas.refetch()
         stakePoolEntries.refetch()
+        analytics.refetch()
       }, 2000)
     )
     setStakedSelected([])
@@ -258,7 +256,7 @@ function Home() {
     const initTxs: { tx: Transaction; signers: Signer[] }[] = []
     for (let step = 0; step < unstakedSelected.length; step++) {
       try {
-        let token = unstakedSelected[step]
+        const token = unstakedSelected[step]
         if (!token || !token.tokenAccount) {
           throw new Error('Token account not set')
         }
@@ -305,13 +303,17 @@ function Home() {
           initTxs.map(({ tx }) => tx),
           {
             signers: initTxs.map(({ signers }) => signers),
+            throwIndividualError: true,
             notificationConfig: {
               message: `Successfully staked`,
               description: 'Stake progress will now dynamically update',
             },
           }
         )
-      } catch (e) {}
+      } catch (e) {
+        setLoadingStake(false)
+        return
+      }
     }
 
     const txs: (Transaction | null)[] = await Promise.all(
@@ -352,7 +354,11 @@ function Home() {
           return stake(connection, wallet as Wallet, {
             stakePoolId: stakePool?.pubkey,
             receiptType:
-              !amount || (amount && amount.eq(new BN(1)))
+              (!amount ||
+                (amount &&
+                  amount.eq(new BN(1)) &&
+                  receiptType === ReceiptType.Receipt)) &&
+              receiptType !== ReceiptType.None
                 ? receiptType
                 : undefined,
             originalMintId: new PublicKey(
@@ -395,6 +401,7 @@ function Home() {
         stakedTokenDatas.refetch()
         allowedTokenDatas.refetch()
         stakePoolEntries.refetch()
+        analytics.refetch()
       }, 2000)
     )
     setStakedSelected([])
@@ -521,6 +528,7 @@ function Home() {
     setTotalStaked(Math.ceil(total).toString())
   }
 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     const fetchData = async () => {
       await totalStakedTokens()
@@ -540,9 +548,9 @@ function Home() {
       }}
     >
       <Head>
-        <title>Cardinal Staking UI</title>
+        <title>{stakePoolMetadata?.displayName ?? 'Cardinal Staking UI'}</title>
         <meta name="description" content="Generated by Cardinal Staking UI" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={stakePoolMetadata?.imageUrl ?? `/favicon.ico`} />
         <script
           defer
           data-domain="stake.cardinal.so"
@@ -591,7 +599,9 @@ function Home() {
                   ? {
                       background: stakePoolMetadata?.colors?.secondary,
                       borderColor: stakePoolMetadata?.colors?.accent,
-                      color: stakePoolMetadata?.colors?.fontColor,
+                      color:
+                        stakePoolMetadata?.colors?.fontColorSecondary ||
+                        stakePoolMetadata?.colors?.fontColor,
                     }
                   : {}
               }
@@ -603,7 +613,7 @@ function Home() {
         )}
         {(maxStaked || rewardDistributorData) && !stakePoolMetadata?.notFound && (
           <div
-            className={`mx-5 mb-4 flex flex-wrap items-center gap-4 rounded-md px-10 py-6  md:flex-row md:justify-between ${
+            className={`mx-5 mb-4 flex flex-wrap items-center gap-4 rounded-md px-10 py-6 md:flex-row md:justify-between ${
               stakePoolMetadata?.colors?.fontColor
                 ? `text-[${stakePoolMetadata?.colors?.fontColor}]`
                 : 'text-gray-200'
@@ -679,6 +689,7 @@ function Home() {
                         rewardDistributorData.data.parsed.rewardMint,
                         environment.label
                       )}
+                      rel="noreferrer"
                     >
                       {rewardMintInfo.data.tokenListData?.name ||
                         rewardMintInfo.data.metaplexMintData?.data.name ||
@@ -701,7 +712,7 @@ function Home() {
                           {formatMintNaturalAmountAsDecimal(
                             rewardMintInfo.data.mintInfo,
                             rewards.data?.claimableRewards,
-                            6
+                            Math.min(rewardMintInfo.data.mintInfo.decimals, 6)
                           )}{' '}
                           {rewardMintInfo.data.tokenListData?.name ||
                             rewardMintInfo.data.metaplexMintData?.data.name ||
@@ -713,13 +724,19 @@ function Home() {
                             ? formatMintNaturalAmountAsDecimal(
                                 rewardMintInfo.data.mintInfo,
                                 rewardMintInfo.data.mintInfo.supply,
-                                6
+                                Math.min(
+                                  rewardMintInfo.data.mintInfo.decimals,
+                                  6
+                                )
                               )
                             : rewardDistributorTokenAccountData.data
                             ? formatMintNaturalAmountAsDecimal(
                                 rewardMintInfo.data.mintInfo,
                                 rewardDistributorTokenAccountData.data?.amount,
-                                6
+                                Math.min(
+                                  rewardMintInfo.data.mintInfo.decimals,
+                                  6
+                                )
                               )
                             : '??'}{' '}
                           Left In Treasury
@@ -774,7 +791,10 @@ function Home() {
               <div className="relative flex flex-grow items-center justify-center">
                 {Object.keys(analytics.data).map((key) => {
                   return (
-                    <div className="relative flex flex-grow items-center justify-center text-lg">
+                    <div
+                      key={key}
+                      className="relative flex flex-grow items-center justify-center text-lg"
+                    >
                       <span
                         className={`${
                           stakePoolMetadata?.colors?.fontColor
@@ -876,7 +896,7 @@ function Home() {
                     <div className="h-[200px] animate-pulse rounded-lg bg-white bg-opacity-5 p-10"></div>
                     <div className="h-[200px] animate-pulse rounded-lg bg-white bg-opacity-5 p-10"></div>
                   </div>
-                ) : (allowedTokenDatas.data || []).length == 0 ? (
+                ) : (allowedTokenDatas.data || []).length === 0 ? (
                   <p
                     className={`font-normal text-[${
                       stakePoolMetadata?.colors?.fontColor
@@ -1036,7 +1056,7 @@ function Home() {
             </div>
 
             <div className="mt-2 flex items-center justify-between gap-5">
-              {!stakePoolMetadata?.receiptType && !showFungibleTokens ? (
+              {!stakePoolMetadata?.receiptType ? (
                 <MouseoverTooltip
                   title={
                     receiptType === ReceiptType.Original
@@ -1398,7 +1418,8 @@ function Home() {
                                                 ? rewardEntries.data.find(
                                                     (entry) =>
                                                       entry.parsed.stakeEntry.equals(
-                                                        tk.stakeEntry?.pubkey!
+                                                        tk.stakeEntry?.pubkey ||
+                                                          PublicKey.default
                                                       )
                                                   )?.parsed.multiplier ||
                                                     rewardDistributorData.data
@@ -1435,7 +1456,8 @@ function Home() {
                                                               (entry) =>
                                                                 entry.parsed.stakeEntry.equals(
                                                                   tk.stakeEntry
-                                                                    ?.pubkey!
+                                                                    ?.pubkey ||
+                                                                    PublicKey.default
                                                                 )
                                                             )?.parsed
                                                               .multiplier ||
@@ -1715,7 +1737,9 @@ function Home() {
           </div>
         </div>
       </div>
-      <Footer bgColor={stakePoolMetadata?.colors?.primary} />
+      {!customHostname && (
+        <Footer bgColor={stakePoolMetadata?.colors?.primary} />
+      )}
     </div>
   )
 }
