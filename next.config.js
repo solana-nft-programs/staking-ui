ca4500e5dc7807c624b9789828e0b530a34b30dd
Giannis Chatziveroglou <gchatz@mit.edu>
2022-09-14 11:40:19 -0700

    Clean next.config.js (#182)

--- next.config.js
+++ next.config.js
@@ -1,32 +1,5 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 
-const HOST_MAPPINGS = [
-  {
-    name: 'rogue-sharks',
-    hostname: 'stake.roguesharks.org',
-  },
-  {
-    name: 'Orbit',
-    hostname: 'stake.unfrgtn.space',
-  },
-  {
-    name: 'degengod',
-    hostname: 'stake.degengod.xyz',
-  },
-  {
-    name: 'plane-x',
-    hostname: 'staking.plane-x.io',
-  },
-  {
-    name: 'lil-weenees',
-    hostname: 'stake.weenee.me',
-  },
-  {
-    name: 'rebellionbots',
-    hostname: 'stake.rebellionbots.io',
-  },
-]
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -34,49 +7,6 @@ const nextConfig = {
     MAINNET_PRIMARY: process.env.MAINNET_PRIMARY,
     MAINNET_SECONDARY: process.env.MAINNET_SECONDARY,
     BASE_CLUSTER: process.env.BASE_CLUSTER,
-  },
-  async rewrites() {
-    return HOST_MAPPINGS.reduce(
-      (acc, mapping) =>
-        mapping.hostname
-          ? [
-              ...acc,
-              {
-                source: '/:path(.*)',
-                destination: `/${mapping.name}`,
-                has: [
-                  {
-                    type: 'host',
-                    value: mapping.hostname,
-                  },
-                ],
-              },
-            ]
-          : acc,
-      []
-    )
-  },
-  async redirects() {
-    return HOST_MAPPINGS.reduce(
-      (acc, mapping) =>
-        mapping.hostname
-          ? [
-              ...acc,
-              {
-                source: '/',
-                destination: `/${mapping.name}`,
-                permanent: false,
-                has: [
-                  {
-                    type: 'host',
-                    value: mapping.hostname,
-                  },
-                ],
-              },
-            ]
-          : acc,
-      []
-    )
   },
 }
 
