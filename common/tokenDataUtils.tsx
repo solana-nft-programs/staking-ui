819932fbc8c7e3375cc6d7209fe3793a8a407f1f
jpbogle <jpbogle22@gmail.com>
2022-09-21 16:14:24 -0700

    Lazy load metadata

---
+++ common/tokenDataUtils.tsx
@@ -0,0 +1,24 @@
+import type { AllowedTokenData } from 'hooks/useAllowedTokenDatas'
+import type { MintMetadata } from 'hooks/useMintMetadata'
+import type { StakeEntryTokenData } from 'hooks/useStakedTokenDatas'
+
+export const getNameFromTokenData = (
+  tokenData: AllowedTokenData | StakeEntryTokenData,
+  mintMetadata?: MintMetadata,
+  defaultName?: string
+) => {
+  return (
+    mintMetadata?.parsed?.name ||
+    tokenData.metaplexData?.data.data.name ||
+    tokenData.tokenListData?.name ||
+    defaultName
+  )
+}
+
+export const getImageFromTokenData = (
+  tokenData: AllowedTokenData | StakeEntryTokenData,
+  mintMetadata?: MintMetadata
+) => {
+  console.log(mintMetadata)
+  return mintMetadata?.parsed?.image || tokenData.tokenListData?.logoURI
+}
