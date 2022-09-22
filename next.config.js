fc75b71bba09d60896c6b938e68756f903d2967f
Pixel y00ts <113155776+DicersN00b@users.noreply.github.com>
2022-09-23 06:15:07 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,96 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 
+const HOST_MAPPINGS = [
+  {
+    name: 'rogue-sharks',
+    hostname: 'stake.roguesharks.org',
+  },
+  {
+    name: 'Orbit',
+    hostname: 'stake.unfrgtn.space',
+  },
+  {
+    name: 'degengod',
+    hostname: 'stake.degengod.xyz',
+  },
+  {
+    name: 'onespace',
+    hostname: 'stake.1space.me',
+  },
+  {
+    name: 'plane-x',
+    hostname: 'staking.plane-x.io',
+  },
+  {
+    name: 'lil-weenees',
+    hostname: 'stake.weenee.me',
+  },
+  {
+    name: 'Pixel-Y00ts',
+    hostname: 'staking.pixely00ts.xyz',
+  },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
     MAINNET_PRIMARY: process.env.MAINNET_PRIMARY,
-    MAINNET_SECONDARY: process.env.MAINNET_SECONDARY,
-    GEO_LOCATION_API_KEY: process.env.GEO_LOCATION_API_KEY,
     BASE_CLUSTER: process.env.BASE_CLUSTER,
+  },
+  async rewrites() {
+    return HOST_MAPPINGS.reduce(
+      (acc, mapping) =>
+        mapping.hostname
+          ? [
+              ...acc,
+              {
+                source: '/:path(.*)',
+                destination: `/${mapping.name}`,
+                has: [
+                  {
+                    type: 'host',
+                    value: mapping.hostname,
+                  },
+                ],
+              },
+            ]
+          : acc,
+      []
+    )
+  },
+  async redirects() {
+    return HOST_MAPPINGS.reduce(
+      (acc, mapping) =>
+        mapping.hostname
+          ? [
+              ...acc,
+              {
+                source: '/',
+                destination: `/${mapping.name}`,
+                permanent: false,
+                has: [
+                  {
+                    type: 'host',
+                    value: mapping.hostname,
+                  },
+                ],
+              },
+            ]
+          : acc,
+      []
+    )
   },
 }
 
-module.exports =
-  process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
-    ? withSentryConfig(nextConfig, {
-        // Additional config options for the Sentry Webpack plugin. Keep in mind that
-        // the following options are set automatically, and overriding them is not
-        // recommended:
-        //   release, url, org, project, authToken, configFile, stripPrefix,
-        //   urlPrefix, include, ignore
+module.exports = withSentryConfig(nextConfig, {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
 
-        silent: true, // Suppresses all logs
-        // For all available options, see:
-        // https://github.com/getsentry/sentry-webpack-plugin#options.
-      })
-    : nextConfig
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+})
