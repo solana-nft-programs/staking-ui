805e5845e3e156046084aff72733694a0ecd4287
jpbogle <jpbogle22@gmail.com>
2022-09-27 17:18:15 -0400

    Default image

--- common/HeroLarge.tsx
+++ common/HeroLarge.tsx
@@ -4,13 +4,18 @@ import { css } from '@emotion/react'
 import { useStakePoolId } from 'hooks/useStakePoolId'
 // import { StakePoolInfo } from 'components/StakePoolInfo'
 import { useStakePoolMetadata } from 'hooks/useStakePoolMetadata'
+import { useRouter } from 'next/router'
+import { useEnvironmentCtx } from 'providers/EnvironmentProvider'
 
 import { HeroStats } from '../components/HeroStats'
+import { ButtonSmall } from './ButtonSmall'
 // import { SocialIcon } from './Socials'
 
 export const HeroLarge: React.FC = () => {
   const stakePoolId = useStakePoolId()
+  const { environment } = useEnvironmentCtx()
   const { data: config } = useStakePoolMetadata()
+  const router = useRouter()
   // if (!config?.description) return <StakePoolInfo />
   return (
     <div className="relative -z-10 flex w-full flex-wrap items-stretch justify-center gap-8 py-8 lg:flex-nowrap lg:justify-between lg:gap-24">
@@ -26,7 +31,7 @@ export const HeroLarge: React.FC = () => {
           background-color: ${config?.colors?.accent};
         `}
       />
-      {config?.imageUrl && (
+      {config?.imageUrl ? (
         <div className="relative flex w-1/4 grow items-center justify-center rounded-xl">
           <img
             className={`max-h-[200px] w-auto rounded-xl ${
@@ -35,6 +40,22 @@ export const HeroLarge: React.FC = () => {
             src={config?.imageUrl}
             alt={config?.displayName}
           />
+        </div>
+      ) : (
+        <div className="flex h-[250px] grow items-center justify-center rounded-xl bg-white bg-opacity-5 md:w-1/4">
+          <ButtonSmall
+            onClick={() => {
+              router.push(
+                `/admin${
+                  environment.label !== 'mainnet-beta'
+                    ? `?cluster=${environment.label}`
+                    : ''
+                }`
+              )
+            }}
+          >
+            Add image
+          </ButtonSmall>
         </div>
       )}
       <div className="flex w-3/4 grow-[2] flex-col py-4">
