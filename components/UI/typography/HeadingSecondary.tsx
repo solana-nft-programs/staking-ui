c2f3e65e2b1b2e5e25338df51d9c92ca0b427c8e
Dan Ward <danward@gmail.com>
2022-12-06 21:51:32 -0800

    Bring in types, tw config from cardinal.so

--- components/UI/typography/HeadingSecondary.tsx
+++ components/UI/typography/HeadingSecondary.tsx
@@ -1,27 +1,28 @@
-import { HeadingElements } from "@/types/index";
-import classNames from "classnames";
+import classNames from 'classnames'
+
+import { HeadingElements } from '@/types/index'
 
 type Props = {
-  children: React.ReactNode;
-  className?: string;
-  el?: HeadingElements;
-};
+  children: React.ReactNode
+  className?: string
+  el?: HeadingElements
+}
 
-const { H2, DIV } = HeadingElements;
+const { H2, DIV } = HeadingElements
 
 const HeadingSecondary = ({ children, className, el = DIV }: Props) => {
   const combinedClasses = classNames([
-    "text-3xl font-medium md:w-full min-w-full",
+    'text-3xl font-medium md:w-full min-w-full',
     className,
-  ]);
+  ])
 
   switch (el) {
     case H2:
-      return <h1 className={combinedClasses}>{children}</h1>;
+      return <h1 className={combinedClasses}>{children}</h1>
     case DIV:
     default:
-      return <div className={combinedClasses}>{children}</div>;
+      return <div className={combinedClasses}>{children}</div>
   }
-};
+}
 
-export default HeadingSecondary;
+export default HeadingSecondary
--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,60 @@ module.exports = {
         'dark-5': '#0B0B0B',
         'dark-6': '#000000',
         twitter: '#1DA1F2',
+        blue: {
+          500: '#29A6F3',
+        },
+        violet: {
+          500: '#CE81F4',
+        },
+        yellow: {
+          500: '#FEFF52',
+        },
+        green: {
+          200: '#E7FE55',
+          500: '#74FF8C',
+        },
+        purple: {
+          400: '#CE81F4',
+          500: '#907EFF',
+        },
+        teal: {
+          500: '#7EFFE8',
+        },
+        orange: {
+          500: '#F2A93C',
+          900: '#3e2807',
+        },
+        gray: {
+          400: '#B1AFBB',
+          500: '#8D8B9B',
+          600: '#252526',
+          700: '#29292a',
+          900: '#030301',
+        },
+        slate: {
+          900: '#0c0c0d',
+        },
+        red: {
+          800: '#4c1734',
+        },
+      },
+      letterSpacing: {
+        wider: '0.07em',
+      },
+      lineHeight: {
+        12: '3.5rem',
+      },
+      leading: {
+        relaxed: '1.5',
+      },
+      boxShadow: {
+        deep: '11px 20px 19px 0px rgba(0,0,0,0.53);',
+        'deep-float': '8px 20px 24px 4px rgba(0,0,0,0.4)',
+        'yellow-glow': '0px 0px 80px rgba(242, 169, 60, 0.30)',
+        'green-glow': '0px 0px 80px rgba(116, 255, 140, 0.2)',
+        'blue-glow': '0px 0px 80px rgba(41, 166, 243, 0.3);',
+        'maroon-glow': '0px 0px 80px rgba(76, 23, 52, 0.5);',
       },
     },
   },
