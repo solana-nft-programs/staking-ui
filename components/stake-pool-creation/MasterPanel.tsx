0635cc50a2cbc50e0547fc6daf53276afedef21f
Dan Ward <danward@gmail.com>
2022-12-07 01:25:04 -0800

    Complete final step indicator with logic

--- components/stake-pool-creation/MasterPanel.tsx
+++ components/stake-pool-creation/MasterPanel.tsx
@@ -5,13 +5,51 @@ import { StepIndicator } from '@/components/stake-pool-creation/master-panel-con
 import { ButtonPrimary } from '@/components/UI/buttons/ButtonPrimary'
 
 export const MasterPanel = () => {
-  const [step, setStep] = useState(0)
+  const [majorStep, setMajorStep] = useState(0)
+  const [minorStep, setMinorStep] = useState(0)
+
+  const incrementMajorStep = () => {
+    setMinorStep(1)
+    setMajorStep(majorStep + 1)
+  }
+
+  const incrementMinorStep = () => setMinorStep(minorStep + 1)
+
+  const handleNextButtonPress = () => {
+    switch (majorStep) {
+      case 0:
+        setMajorStep(1)
+        setMinorStep(1)
+        break
+      case 1:
+        minorStep === 4 ? incrementMajorStep() : incrementMinorStep()
+        break
+      case 2:
+        minorStep === 3 ? incrementMajorStep() : incrementMinorStep()
+        break
+      case 3:
+        minorStep === 3 ? incrementMajorStep() : incrementMinorStep()
+        break
+      case 4:
+        minorStep === 4 ? incrementMajorStep() : incrementMinorStep()
+        break
+      case 5:
+        minorStep === 4 ? incrementMajorStep() : incrementMinorStep()
+        break
+      default:
+        break
+    }
+  }
 
   return (
     <div className="w-1/3 space-y-6">
-      {step > 0 && <StepIndicator currentStep={step} />}
-      <Intro />
-      <ButtonPrimary onClick={() => setStep(step + 1)}>Start</ButtonPrimary>
+      {majorStep === 0 && <Intro />}
+      {minorStep > 0 && <StepIndicator currentStep={majorStep} />}
+      <div className="flex">
+        <ButtonPrimary onClick={handleNextButtonPress}>
+          {majorStep === 0 ? 'Start' : 'Next'}
+        </ButtonPrimary>
+      </div>
     </div>
   )
 }
