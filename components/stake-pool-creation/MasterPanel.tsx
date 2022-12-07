5b0cec0a5e0b365f7b8a407af0076ea5fbaa7600
Dan Ward <danward@gmail.com>
2022-12-07 00:46:07 -0800

    Add step indicator component

--- components/stake-pool-creation/MasterPanel.tsx
+++ components/stake-pool-creation/MasterPanel.tsx
@@ -1,46 +1,17 @@
-import { FlagIcon } from 'assets/icons/FlagIcon'
-import { GasPumpIcon } from 'assets/icons/GasPumpIcon'
-import { SettingsIcon } from 'assets/icons/SettingsIcon'
-import { HeadingPrimary } from 'components/UI/typography/HeadingPrimary'
+import { useState } from 'react'
 
+import { Intro } from '@/components/stake-pool-creation/master-panel-content/Intro'
+import { StepIndicator } from '@/components/stake-pool-creation/master-panel-content/step-indicator/StepIndicator'
 import { ButtonPrimary } from '@/components/UI/buttons/ButtonPrimary'
-import { HorizontalDivider } from '@/components/UI/HorizontalDivider'
-import { BodyCopy } from '@/components/UI/typography/BodyCopy'
 
 export const MasterPanel = () => {
+  const [step, setStep] = useState(0)
+
   return (
     <div className="w-1/3 space-y-6">
-      <HeadingPrimary>Create your Staking Pool</HeadingPrimary>
-      <BodyCopy>Thank you for your interest!</BodyCopy>
-      <BodyCopy>
-        Adding utility like staking brings rewards for both the users and the
-        NFT collection itself.
-      </BodyCopy>
-      <div className="py-8">
-        <HorizontalDivider />
-      </div>
-      <BodyCopy>You will be guided through the whole process:</BodyCopy>
-      <div className="flex flex-wrap space-y-2 pb-8">
-        <div className="flex w-full rounded-xl bg-gray-600 p-6">
-          <div className="mr-4">
-            <FlagIcon />
-          </div>
-          Complete 6 simple steps
-        </div>
-        <div className="flex w-full rounded-xl bg-gray-600 p-6">
-          <div className="mr-4">
-            <GasPumpIcon />
-          </div>
-          You will only be charged with blockchain gas fees
-        </div>
-        <div className="flex w-full rounded-xl bg-gray-600 p-6">
-          <div className="mr-4">
-            <SettingsIcon />
-          </div>
-          Manage your Stake Pool after creation
-        </div>
-      </div>
-      <ButtonPrimary onClick={() => {}}>Start</ButtonPrimary>
+      {step > 0 && <StepIndicator currentStep={step} />}
+      <Intro />
+      <ButtonPrimary onClick={() => setStep(step + 1)}>Start</ButtonPrimary>
     </div>
   )
 }
---
+++ components/stake-pool-creation/master-panel-content/step-indicator/StepIndicator.tsx
@@ -0,0 +1,22 @@
+import { StepIndicatorBubble } from '@/components/stake-pool-creation/master-panel-content/step-indicator/StepIndicatorBubble'
+import { HorizontalDivider } from '@/components/UI/HorizontalDivider'
+
+export type StepIndicatorProps = {
+  currentStep: number
+}
+
+const numberOfSteps = 7
+
+export const StepIndicator = ({ currentStep }: StepIndicatorProps) => {
+  return (
+    <div className="relative flex items-center justify-between py-2">
+      <div className="absolute w-full px-1">
+        <HorizontalDivider />
+      </div>
+      {}
+      {[...Array(numberOfSteps)].map((_, index) => (
+        <StepIndicatorBubble isActive={index + 1 === currentStep} key={index} />
+      ))}
+    </div>
+  )
+}
