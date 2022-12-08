74bf6ccf8647b122117b573d4623283570c1d370
Dan Ward <danward@gmail.com>
2022-12-07 21:49:22 -0800

    Refactor steps mechanism

--- components/stake-pool-creation/SlavePanel.tsx
+++ components/stake-pool-creation/SlavePanel.tsx
@@ -1,22 +1,19 @@
 import { Intro } from '@/components/stake-pool-creation/slave-panel-content/Intro'
 import { HeadingPrimary } from '@/components/UI/typography/HeadingPrimary'
-import { HeadingSecondary } from '@/components/UI/typography/HeadingSecondary'
 
 export type SlavePanelProps = {
-  majorStep: number
-  minorStep: number
+  currentStep: number
 }
 
-export const SlavePanel = ({ majorStep, minorStep }: SlavePanelProps) => {
+export const SlavePanel = ({ currentStep }: SlavePanelProps) => {
   return (
     <div className="ml-8 flex w-3/5 flex-col">
       <div className="relative flex h-full w-full flex-col items-center justify-center overflow-clip rounded-2xl bg-black py-16">
-        {majorStep === 0 ? (
+        {currentStep === 0 ? (
           <Intro />
         ) : (
           <>
-            <HeadingPrimary className="mb-8">Step {majorStep}</HeadingPrimary>
-            <HeadingSecondary>Sub-step {minorStep}</HeadingSecondary>
+            <HeadingPrimary className="mb-8">Step {currentStep}</HeadingPrimary>
           </>
         )}
       </div>
--- components/stake-pool-creation/master-panel/MasterPanel.tsx
+++ components/stake-pool-creation/master-panel/MasterPanel.tsx
@@ -8,13 +8,11 @@ import { HeadingPrimary } from '@/components/UI/typography/HeadingPrimary'
 import { ButtonWidths } from '@/types/index'
 
 export type MasterPanelProps = {
-  majorStep: number
-  minorStep: number
-  setMajorStep: (step: number) => void
-  setMinorStep: (step: number) => void
+  currentStep: number
+  setCurrentStep: (step: number) => void
 }
 
-const majorStepTitles = [
+const stepTitles = [
   'Create your Staking Pool',
   'Authorization',
   'Reward distribution',
@@ -24,7 +22,7 @@ const majorStepTitles = [
   'Summary',
 ]
 
-const minorStepTitles = [
+const stepSubtitles = [
   'Thank you for your interest!',
   'Decide which NFT collections or coins will be staked.',
   'Specify the emission and source of rewards for stakers.',
@@ -35,130 +33,44 @@ const minorStepTitles = [
 ]
 
 export const MasterPanel = ({
-  majorStep,
-  minorStep,
-  setMajorStep,
-  setMinorStep,
+  currentStep,
+  setCurrentStep,
 }: MasterPanelProps) => {
-  const [majorStepTitle, setMajorStepTitle] = useState('')
+  const [title, setTitle] = useState('')
   const [stepSubtitle, setStepSubtitle] = useState('')
 
-  const incrementMajorStep = () => {
-    setMinorStep(1)
-    setMajorStep(majorStep + 1)
-  }
-
-  const incrementMinorStep = () => setMinorStep(minorStep + 1)
-  const decrementMinorStep = () => setMinorStep(minorStep - 1)
-
-  const handlePreviousButtonPress = () => {
-    switch (majorStep) {
-      case 0:
-        break
-      case 1:
-        minorStep > 1 && setMinorStep(minorStep - 1)
-        break
-      case 2:
-        if (minorStep === 1) {
-          setMajorStep(majorStep - 1)
-          setMinorStep(4)
-        } else {
-          decrementMinorStep()
-        }
-        break
-      case 3:
-        if (minorStep === 1) {
-          setMajorStep(majorStep - 1)
-          setMinorStep(3)
-        } else {
-          decrementMinorStep()
-        }
-        break
-      case 4:
-        if (minorStep === 1) {
-          setMajorStep(majorStep - 1)
-          setMinorStep(3)
-        } else {
-          decrementMinorStep()
-        }
-        break
-      case 5:
-        if (minorStep === 1) {
-          setMajorStep(majorStep - 1)
-          setMinorStep(4)
-        } else {
-          decrementMinorStep()
-        }
-        break
-      case 6:
-        if (minorStep === 1) {
-          setMajorStep(majorStep - 1)
-          setMinorStep(4)
-        } else {
-          decrementMinorStep()
-        }
-        break
-    }
-  }
-
-  const handleNextButtonPress = () => {
-    switch (majorStep) {
-      case 0:
-        setMajorStep(1)
-        setMinorStep(1)
-        break
-      case 1:
-        minorStep === 4 ? incrementMajorStep() : incrementMinorStep()
-        break
-      case 2:
-        minorStep === 3 ? incrementMajorStep() : incrementMinorStep()
-        break
-      case 3:
-        minorStep === 3 ? incrementMajorStep() : incrementMinorStep()
-        break
-      case 4:
-        minorStep === 4 ? incrementMajorStep() : incrementMinorStep()
-        break
-      case 5:
-        minorStep === 4 ? incrementMajorStep() : incrementMinorStep()
-        break
-      default:
-        break
-    }
-  }
-
   useEffect(() => {
-    const title = majorStepTitles?.[majorStep]
-    const subTitle = minorStepTitles?.[majorStep]
+    const title = stepTitles?.[currentStep]
+    const subTitle = stepSubtitles?.[currentStep]
     if (!title || !subTitle) return
-    setMajorStepTitle(title)
+    setTitle(title)
     setStepSubtitle(subTitle)
-  }, [majorStep])
+  }, [currentStep])
 
   return (
     <div className="w-2/5 space-y-2">
-      <HeadingPrimary>{majorStepTitle}</HeadingPrimary>
+      <HeadingPrimary>{title}</HeadingPrimary>
       <BodyCopy className="pb-2">{stepSubtitle}</BodyCopy>
-      {minorStep > 0 && (
+      {currentStep > 0 && (
         <div className=" pb-16">
-          <StepIndicator currentStep={majorStep} />
+          <StepIndicator currentStep={currentStep} />
         </div>
       )}
-      <StepContent majorStep={majorStep} minorStep={minorStep} />
+      <StepContent currentStep={currentStep} />
       <div className="flex space-x-4">
-        {majorStep > 0 && (
+        {currentStep > 0 && (
           <ButtonPrimary
-            onClick={handlePreviousButtonPress}
+            onClick={() => setCurrentStep(currentStep - 1)}
             width={ButtonWidths.NARROW}
           >
             Previous
           </ButtonPrimary>
         )}
         <ButtonPrimary
-          onClick={handleNextButtonPress}
+          onClick={() => setCurrentStep(currentStep + 1)}
           width={ButtonWidths.NARROW}
         >
-          {majorStep === 0 ? 'Start' : 'Next'}
+          {currentStep === 0 ? 'Start' : 'Next'}
         </ButtonPrimary>
       </div>
     </div>
