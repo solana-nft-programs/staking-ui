e203511558306b20af7565beb5988b0f294d717a
Dan Ward <danward@gmail.com>
2022-12-08 23:06:47 -0800

    Improve auth slave screens

--- components/stake-pool-creation/slave-panel-content/authorization/TipTwo.tsx
+++ components/stake-pool-creation/slave-panel-content/authorization/TipTwo.tsx
@@ -1,25 +1,23 @@
-import { BodyCopy } from '@/components/UI/typography/BodyCopy'
 import { InformationCircleIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
+
+import { BodyCopy } from '@/components/UI/typography/BodyCopy'
 
 export const TipTwo = () => {
   return (
     <>
-      <div className="mb-6">
-        <div className="pr-16">
-          <Image
-            src="/images/stake-pool-creation/authorization/tip-two.png"
-            height={800}
-            width={800}
-            objectFit="contain"
-          />
-        </div>
-        <BodyCopy className="flex items-center justify-center text-center">
-          <InformationCircleIcon className="mr-2 inline-block h-6 w-6" />
-          Allow for staking any tokens with specified by you creator
-          address(es).
-        </BodyCopy>
+      <div className="xl:pr-40">
+        <Image
+          src="/images/stake-pool-creation/authorization/tip-two.png"
+          height={800}
+          width={800}
+          objectFit="contain"
+        />
       </div>
+      <BodyCopy className="flex items-center justify-center text-center">
+        <InformationCircleIcon className="mr-2 inline-block h-6 w-6" />
+        Allow for staking any tokens with specified by you creator address(es).
+      </BodyCopy>
     </>
   )
 }
