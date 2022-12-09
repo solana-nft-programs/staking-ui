69e994da89f9579a9e047171c492884d70fd49fe
Dan Ward <danward@gmail.com>
2022-12-08 23:02:49 -0800

    Build out authorization slave screens

--- components/stake-pool-creation/slave-panel-content/ContentPrimary.tsx
+++ components/stake-pool-creation/slave-panel-content/ContentPrimary.tsx
@@ -1,4 +1,6 @@
 import { TipOne } from '@/components/stake-pool-creation/slave-panel-content/authorization/TipOne'
+import { TipThree } from '@/components/stake-pool-creation/slave-panel-content/authorization/TipThree'
+import { TipTwo } from '@/components/stake-pool-creation/slave-panel-content/authorization/TipTwo'
 import { Intro } from '@/components/stake-pool-creation/slave-panel-content/Intro'
 import { SlavePanelScreens } from '@/components/stake-pool-creation/SlavePanel'
 
@@ -7,12 +9,15 @@ export type ContentPrimaryProps = {
 }
 
 export const ContentPrimary = ({ activeScreen }: ContentPrimaryProps) => {
-  const { INTRO, AUTHORIZATION_1 } = SlavePanelScreens
+  const { INTRO, AUTHORIZATION_1, AUTHORIZATION_2, AUTHORIZATION_3 } =
+    SlavePanelScreens
 
   return (
     <div className="z-10 flex flex-col items-center">
       {activeScreen === INTRO && <Intro />}
       {activeScreen === AUTHORIZATION_1 && <TipOne />}
+      {activeScreen === AUTHORIZATION_2 && <TipTwo />}
+      {activeScreen === AUTHORIZATION_3 && <TipThree />}
     </div>
   )
 }
