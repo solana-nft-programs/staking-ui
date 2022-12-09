bac63b508c772b19e3110db9ab3814d9ff6c36e5
Dan Ward <danward@gmail.com>
2022-12-08 22:03:46 -0800

    Add enum for all slave panel screens

--- components/stake-pool-creation/slave-panel-content/ContentPrimary.tsx
+++ components/stake-pool-creation/slave-panel-content/ContentPrimary.tsx
@@ -1,13 +1,16 @@
 import { Intro } from '@/components/stake-pool-creation/slave-panel-content/Intro'
+import { SlavePanelScreens } from '@/components/stake-pool-creation/SlavePanel'
 
 export type ContentPrimaryProps = {
-  currentStep: number
+  activeScreen: SlavePanelScreens
 }
 
-export const ContentPrimary = ({ currentStep }: ContentPrimaryProps) => {
+export const ContentPrimary = ({ activeScreen }: ContentPrimaryProps) => {
+  const { INTRO } = SlavePanelScreens
+
   return (
     <div className="z-10 flex flex-col items-center">
-      {currentStep === 0 && <Intro />}
+      {activeScreen === INTRO && <Intro />}
     </div>
   )
 }
