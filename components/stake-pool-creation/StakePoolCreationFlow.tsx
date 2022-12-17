01a18ea62726a79530469d8d77fc9fa82a552718
jpbogle <jpbogle22@gmail.com>
2022-12-16 22:45:36 -0500

    Step indicator spacing

--- components/stake-pool-creation/StakePoolCreationFlow.tsx
+++ components/stake-pool-creation/StakePoolCreationFlow.tsx
@@ -209,70 +209,56 @@ export const StakePoolCreationFlow = ({
         <SuccessPanel />
       </div>
     )
-  } else {
-    return (
-      <div className="w-full px-10">
-        <div className="mb-8 flex w-full py-8">
-          <MasterPanel
-            type={type}
-            submitDisabled={submitDisabled}
-            mintInfo={mintInfo}
-            currentStep={currentStep}
-            setCurrentStep={setCurrentStep}
-            setActiveSlavePanelScreen={setActiveSlavePanelScreen}
-            formState={formState}
-          />
-          <SlavePanel activeScreen={activeSlavePanelScreen} />
-        </div>
-        {currentStep === 0 &&
-          stakePooldForAdmin.data &&
-          stakePooldForAdmin.data?.length > 0 && (
-            <>
-              <div className="text-2xl font-medium">My Stake Pools</div>
-              <div className="grid grid-cols-3 gap-5 py-10">
-                {stakePoolsWithMetadata
-                  .concat(stakePoolsWithoutMetadata)
-                  .map((stakePool) => (
-                    <div
-                      key={stakePool.stakePoolData.pubkey.toString()}
-                      className="h-[300px] cursor-pointer rounded-lg bg-white bg-opacity-5 p-10 transition-all duration-100 hover:scale-[1.01]"
-                      onClick={() => {
-                        window.open(
-                          `/admin/${
-                            stakePool.stakePoolMetadata?.name ||
-                            stakePool.stakePoolData.pubkey.toString()
-                          }${
-                            environment.label !== 'mainnet-beta'
-                              ? `?cluster=${environment.label}`
-                              : ''
-                          }`,
-                          '_blank',
-                          'noopener,noreferrer'
-                        )
-                      }}
-                    >
-                      {stakePool.stakePoolMetadata?.displayName ? (
-                        <div className="text-center font-bold">
-                          {stakePool.stakePoolMetadata?.displayName}
-                        </div>
-                      ) : (
-                        <div className="text-center font-bold text-white">
-                          <a
-                            className="text-white"
-                            target="_blank"
-                            rel="noreferrer"
-                            href={pubKeyUrl(
-                              stakePool.stakePoolData.pubkey,
-                              environment.label
-                            )}
-                          >
-                            {shortPubKey(stakePool.stakePoolData.pubkey)}
-                          </a>
-                        </div>
-                      )}
-                      <div className="text-gray text-center">
+  }
+  return (
+    <div className="w-full px-10">
+      <div className="mb-8 flex w-full py-8">
+        <MasterPanel
+          type={type}
+          submitDisabled={submitDisabled}
+          mintInfo={mintInfo}
+          currentStep={currentStep}
+          setCurrentStep={setCurrentStep}
+          setActiveSlavePanelScreen={setActiveSlavePanelScreen}
+          formState={formState}
+        />
+        <SlavePanel activeScreen={activeSlavePanelScreen} />
+      </div>
+      {currentStep === 0 &&
+        stakePooldForAdmin.data &&
+        stakePooldForAdmin.data?.length > 0 && (
+          <>
+            <div className="text-2xl font-medium">My Stake Pools</div>
+            <div className="grid grid-cols-3 gap-5 py-10">
+              {stakePoolsWithMetadata
+                .concat(stakePoolsWithoutMetadata)
+                .map((stakePool) => (
+                  <div
+                    key={stakePool.stakePoolData.pubkey.toString()}
+                    className="h-[300px] cursor-pointer rounded-lg bg-white bg-opacity-5 p-10 transition-all duration-100 hover:scale-[1.01]"
+                    onClick={() => {
+                      window.open(
+                        `/admin/${
+                          stakePool.stakePoolMetadata?.name ||
+                          stakePool.stakePoolData.pubkey.toString()
+                        }${
+                          environment.label !== 'mainnet-beta'
+                            ? `?cluster=${environment.label}`
+                            : ''
+                        }`,
+                        '_blank',
+                        'noopener,noreferrer'
+                      )
+                    }}
+                  >
+                    {stakePool.stakePoolMetadata?.displayName ? (
+                      <div className="text-center font-bold">
+                        {stakePool.stakePoolMetadata?.displayName}
+                      </div>
+                    ) : (
+                      <div className="text-center font-bold text-white">
                         <a
-                          className="text-xs text-gray-500"
+                          className="text-white"
                           target="_blank"
                           rel="noreferrer"
                           href={pubKeyUrl(
@@ -283,29 +269,42 @@ export const StakePoolCreationFlow = ({
                           {shortPubKey(stakePool.stakePoolData.pubkey)}
                         </a>
                       </div>
-                      {stakePool.stakePoolMetadata?.imageUrl ? (
-                        <img
-                          className="mx-auto mt-5 h-[150px] w-[150px] rounded-md"
-                          src={stakePool.stakePoolMetadata.imageUrl}
-                          alt={stakePool.stakePoolMetadata.name}
-                        />
-                      ) : (
-                        <div className="flex justify-center align-middle">
-                          <div className="mt-5 flex h-[100px] w-[100px] items-center justify-center rounded-full text-5xl text-white text-opacity-40">
-                            <img
-                              className="mx-auto mt-5 h-[100px] w-[100px] rounded-md"
-                              src={'/cardinal-crosshair.svg'}
-                              alt={stakePool.stakePoolData.pubkey.toString()}
-                            />
-                          </div>
-                        </div>
-                      )}
+                    )}
+                    <div className="text-gray text-center">
+                      <a
+                        className="text-xs text-gray-500"
+                        target="_blank"
+                        rel="noreferrer"
+                        href={pubKeyUrl(
+                          stakePool.stakePoolData.pubkey,
+                          environment.label
+                        )}
+                      >
+                        {shortPubKey(stakePool.stakePoolData.pubkey)}
+                      </a>
                     </div>
-                  ))}
-              </div>
-            </>
-          )}
-      </div>
-    )
-  }
+                    {stakePool.stakePoolMetadata?.imageUrl ? (
+                      <img
+                        className="mx-auto mt-5 h-[150px] w-[150px] rounded-md"
+                        src={stakePool.stakePoolMetadata.imageUrl}
+                        alt={stakePool.stakePoolMetadata.name}
+                      />
+                    ) : (
+                      <div className="flex justify-center align-middle">
+                        <div className="mt-5 flex h-[100px] w-[100px] items-center justify-center rounded-full text-5xl text-white text-opacity-40">
+                          <img
+                            className="mx-auto mt-5 h-[100px] w-[100px] rounded-md"
+                            src={'/cardinal-crosshair.svg'}
+                            alt={stakePool.stakePoolData.pubkey.toString()}
+                          />
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                ))}
+            </div>
+          </>
+        )}
+    </div>
+  )
 }
--- components/stake-pool-creation/master-panel/step-indicator/StepIndicatorBubble.tsx
+++ components/stake-pool-creation/master-panel/step-indicator/StepIndicatorBubble.tsx
@@ -15,14 +15,17 @@ export const StepIndicatorBubble = ({
       className={classNames([
         'relative z-10 rounded-full ',
         isActive ? 'bg-white' : 'bg-gray-500',
-        isPast ? 'h-7 w-7 bg-teal-500' : 'h-3 w-3',
+        isPast ? 'h-3 w-3 bg-teal-500' : 'h-3 w-3',
       ])}
     >
       {!!isActive && (
         <div className="absolute -top-2.5 -left-2.5 h-8 w-8 rounded-full border border-white" />
       )}
       {!!isPast && (
-        <CheckIcon className="absolute top-1 left-1 h-5 w-5 text-black" />
+        <>
+          <div className="absolute -top-2.5 -left-2.5 h-8 w-8 rounded-full bg-teal-500" />
+          <CheckIcon className="absolute -top-1 -left-1 h-5 w-5 text-black" />
+        </>
       )}
     </div>
   )
