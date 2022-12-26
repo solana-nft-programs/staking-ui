d65279b05c29c85fc881525044518c84bc93fbf8
Dan Ward <danward@gmail.com>
2022-12-26 15:36:04 -0500

    Merge common logic for boost stat to shared component, util

---
+++ components/token-staking/token-stats/utils.ts
@@ -0,0 +1,33 @@
+import type { IdlAccountData } from '@cardinal/rewards-center'
+import { PublicKey } from '@solana/web3.js'
+import { formatAmountAsDecimal } from 'common/units'
+import type { StakeEntryTokenData } from 'hooks/useStakedTokenDatas'
+
+export interface calculateBoostArgs {
+  rewardDistributorData: Pick<
+    IdlAccountData<'rewardDistributor'>,
+    'pubkey' | 'parsed'
+  >
+  tokenData: StakeEntryTokenData
+  rewardEntriesData:
+    | Pick<IdlAccountData<'rewardEntry'>, 'pubkey' | 'parsed'>[]
+    | undefined
+}
+
+export const calculateBoost = ({
+  rewardDistributorData,
+  rewardEntriesData,
+  tokenData,
+}: calculateBoostArgs) => {
+  return formatAmountAsDecimal(
+    rewardDistributorData?.parsed.multiplierDecimals || 0,
+    rewardEntriesData
+      ? rewardEntriesData.find((entry) =>
+          entry.parsed?.stakeEntry.equals(
+            tokenData.stakeEntry?.pubkey || PublicKey.default
+          )
+        )?.parsed?.multiplier || rewardDistributorData.parsed.defaultMultiplier
+      : rewardDistributorData.parsed.defaultMultiplier,
+    rewardDistributorData.parsed.multiplierDecimals
+  )
+}
