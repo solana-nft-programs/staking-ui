ae29be4d66aa19b6630baf7f6688648c6350559f
Dan Ward <danward@gmail.com>
2022-12-26 18:59:55 -0500

    Extract token wrapper to own component

--- components/token-staking/token-list/TokenListLoader.tsx
+++ components/token-staking/token-list/TokenListLoader.tsx
@@ -3,7 +3,6 @@ export const TokenListLoader = ({}) => {
     <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
       <div className="aspect-square animate-pulse rounded-lg bg-white bg-opacity-5 p-10"></div>
       <div className="aspect-square animate-pulse rounded-lg bg-white bg-opacity-5 p-10"></div>
-      <div className="aspect-square animate-pulse rounded-lg bg-white bg-opacity-5 p-10"></div>
     </div>
   )
 }
