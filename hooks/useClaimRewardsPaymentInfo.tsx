7e3f8dbc6c636353cfdef69774e619d366e26b5f
Dan Ward <danward@gmail.com>
2023-01-05 16:47:14 -0800

    Fix calculation of decimals for fee

--- hooks/useClaimRewardsPaymentInfo.tsx
+++ hooks/useClaimRewardsPaymentInfo.tsx
@@ -1,7 +1,7 @@
 import { fetchIdlAccount } from '@cardinal/rewards-center'
 import type { BN } from '@project-serum/anchor'
 import { SOLANA_TOKEN_MINT_ADDRESS } from 'api/constants'
-import { formatMintNaturalAmountAsDecimal } from 'common/units'
+import { formatAmountAsDecimal } from 'common/units'
 import { useRewardDistributorData } from 'hooks/useRewardDistributorData'
 import { useRewardMintInfo } from 'hooks/useRewardMintInfo'
 import { useTokenList } from 'hooks/useTokenList'
@@ -55,18 +55,20 @@ export const useClaimRewardsPaymentInfo = () => {
             )
           })?.symbol
 
-      const amount = formatMintNaturalAmountAsDecimal(
-        rewardMintInfo.mintInfo, // needs to be from claimRewardsPaymentInfo.parsed.paymentMint
-        claimRewardsPaymentInfo.parsed.paymentAmount,
-        rewardMintInfo.mintInfo?.decimals // needs to be from claimRewardsPaymentInfo.parsed.paymentMint
-      )
-      console.log('claimRewardsPaymentInfo', claimRewardsPaymentInfo.parsed)
+      const decimals = isSol
+        ? 9
+        : tokenList.data.find((token) => {
+            return (
+              token.address ===
+              claimRewardsPaymentInfo.parsed.paymentMint.toString()
+            )
+          })?.decimals || 0
 
-      // const mintInfo = await getMint(
-      //   connection,
-      //   claimRewardsPaymentInfo.parsed.paymentMint
-      // )
-      // console.log('mintInfo', mintInfo)
+      const amount = formatAmountAsDecimal(
+        decimals,
+        claimRewardsPaymentInfo.parsed.paymentAmount,
+        9
+      )
 
       return {
         naturalAmount: claimRewardsPaymentInfo.parsed.paymentAmount,
