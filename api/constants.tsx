ab1a7f87e5c231bf6e0743e4b6fa259a7e829612
Dan Ward <danward@gmail.com>
2023-01-23 10:00:27 -0800

    Remove unused code

--- api/constants.tsx
+++
@@ -1,5 +0,0 @@
-import { PublicKey } from '@solana/web3.js'
-
-export const STAKE_POOL_ID = new PublicKey(
-  'BeunK2rKRNXbL6YsMkKDPD4f24Ms4dcj2JpsN6KCjBjY'
-)
--- hooks/useMintSymbol.tsx
+++ hooks/useMintSymbol.tsx
@@ -1,10 +1,8 @@
 import { PublicKey } from '@solana/web3.js'
-import { useMintInfo } from 'hooks/useMintInfo'
 import { useTokenList } from 'hooks/useTokenList'
 import { useQuery } from 'react-query'
 
 export const useMintSymbol = (mint: PublicKey | undefined) => {
-  const mintInfo = useMintInfo(mint)
   const isSol = mint?.toString() === PublicKey.default.toString()
   const tokenList = useTokenList()
 
@@ -20,7 +18,7 @@ export const useMintSymbol = (mint: PublicKey | undefined) => {
       return token?.symbol
     },
     {
-      enabled: mintInfo.isFetched && tokenList.isFetched && !!mint,
+      enabled: tokenList.isFetched && !!mint,
     }
   )
 }
