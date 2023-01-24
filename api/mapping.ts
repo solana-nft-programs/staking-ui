0271eb699c4fb1f662c98e6b9197e496d882954e
KnucklesButRich <114955844+KnucklesButRich@users.noreply.github.com>
2023-01-24 21:59:07 +0300

    Shibonk Gang Staking Pool Customization

--- api/mapping.ts
+++ api/mapping.ts
@@ -3378,5 +3378,45 @@ export const stakePoolMetadatas: StakePoolMetadata[] = [
     fontColorTertiary: '#7F1B1B',
    },
 },
+  {
+  name: "shibonk-gang",
+  displayName: "Shibonk Gang",
+  stakePoolAddress: "GfJiPMZWegzAywkJpbGDfcMqTbZpmjjX8m5PV5YX67dY",
+  receiptType: ReceiptType.Original,
+  tokenStandard: TokenStandard.NonFungible,
+  hidden: false,
+  notFound: false,
+  hostname: "stake.shibonkcoin.com",
+  hideAllowedTokens: false,
+  styles: {
+    fontFamily: 'Industry, sans-serif',
+    fontWeight: 700,
+    fontSize: '22px',
+    font: '#000000',
+    color: '#000000',
+  },
+  colors: {
+    primary: "white",
+    secondary: "grey",
+    accent: "purple",
+    fontColor: "black",
+    fontColorSecondary: "black",
+    backgroundSecondary: "white"
+  },
+  imageUrl: "https://raw.githubusercontent.com/Sperlo64/SHIBONK/main/SHIBONK%20GANG%20LOGO.png",
+  backgroundBannerImageUrl: "https://raw.githubusercontent.com/Sperlo64/SHIBONK/main/Banner%20final.png",
+  websiteUrl: "https://nft.shibonkcoin.com",
+  maxStaked: 4200,
+  links: [
+    {
+        text: "Mint Now",
+        value: "https://nft.shibonkcoin.com/mint"
+    },
+    {
+        text: "Trade Now",
+        value: "https://magiceden.io/marketplace/6xSm3wN3aU2bApS3mjbj9r2dZbNYe9K1E1E3E96tp9pi"
+    },
+  ]
+},
 ]
 
