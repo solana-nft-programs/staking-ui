5e9dc0c80cb742989abcca071238a81b8f941352
jpbogle <jpbogle22@gmail.com>
2023-02-08 12:36:38 -0500

    Fee info spacing

--- components/fee-info/FeeInfo.tsx
+++ components/fee-info/FeeInfo.tsx
@@ -5,19 +5,11 @@ import { UnstakeFee } from '@/components/fee-info/UnstakeFee'
 
 export const FeeInfo: React.FC = () => {
   return (
-    <div className="-mx-2 flex flex-wrap">
-      <div className="w-full p-2 md:w-1/2 xl:w-auto">
-        <PoolVersionIndicator />
-      </div>
-      <div className="w-full p-2 md:w-1/2 xl:w-auto">
-        <RewardClaimFee />
-      </div>
-      <div className="w-full p-2 md:w-1/2 xl:w-auto">
-        <StakeFee />
-      </div>
-      <div className="w-full p-2 md:w-1/2 xl:w-auto">
-        <UnstakeFee />
-      </div>
+    <div className="flex flex-wrap gap-4">
+      <PoolVersionIndicator />
+      <RewardClaimFee />
+      <StakeFee />
+      <UnstakeFee />
     </div>
   )
 }
