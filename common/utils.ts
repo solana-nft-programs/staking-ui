e3d7f7840f61dcae0c89150db10b3ee3ebe952b5
jpbogle <jpbogle22@gmail.com>
2023-06-20 22:24:37 -0400

    Remove unused utils

--- common/utils.ts
+++ common/utils.ts
@@ -29,28 +29,6 @@ export function metadataUrl(
   }`
 }
 
-export function shortDateString(utc_seconds: number) {
-  return `${new Date(utc_seconds * 1000).toLocaleDateString([], {
-    month: '2-digit',
-    day: '2-digit',
-    year: '2-digit',
-  })} ${new Date(utc_seconds * 1000).toLocaleTimeString([], {
-    hour: '2-digit',
-    minute: '2-digit',
-  })}`
-}
-
-export function longDateString(utcSeconds: number) {
-  return new Date(utcSeconds * 1000).toLocaleTimeString(['en-US'], {
-    year: '2-digit',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    timeZoneName: 'short',
-  })
-}
-
 export function secondstoDuration(durationSeconds: number) {
   const years = Math.floor(durationSeconds / 31536000)
   const months = Math.floor((durationSeconds % 31536000) / 2592000)
@@ -73,11 +51,6 @@ export function secondstoDuration(durationSeconds: number) {
   return duration
 }
 
-export const withSleep = async (fn: () => any, sleep = 2000) => {
-  await new Promise((r) => setTimeout(r, sleep))
-  await fn()
-}
-
 export const firstParam = (param: string | string[] | undefined): string => {
   if (!param) return ''
   return typeof param === 'string' ? param : param[0] || ''
@@ -90,13 +63,6 @@ export const camelCase = (str: string) => {
     .join('')
 }
 
-export const getMintsDetails = async () =>
-  await fetch(
-    'https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json'
-  )
-    .then((response) => response.json())
-    .then((data) => data['tokens'])
-
 export const tryPublicKey = (
   publicKeyString: PublicKey | string | string[] | undefined | null
 ): PublicKey | null => {
@@ -107,16 +73,6 @@ export const tryPublicKey = (
   } catch (e) {
     return null
   }
-}
-
-export function getLink(path: string, withParams = true) {
-  return `${window.location.origin}${path}${
-    withParams
-      ? path.includes('?') && window.location.search
-        ? `${window.location.search.replace('?', '&')}`
-        : window.location.search ?? ''
-      : ''
-  }`
 }
 
 export const hexColor = (colorString: string): string => {
